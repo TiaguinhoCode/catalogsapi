8227e6b2c5c333f3d976e33b036f488b3f9428b0
unknown <tiago.rafael019@gmail.com>
2025-01-29 07:31:18 -0300

    Filtro do detalhamento do usuario por id

--- src/controllers/user/listUsersController.ts
+++ src/controllers/user/listUsersController.ts
@@ -7,10 +7,11 @@ import { ListUsersService } from "../../Services/user/listUsersService";
 class ListUsersController {
   async handle(req: Request, res: Response) {
     const company = (req.query.company as string) || "nenhum";
+    const id = req.query.id as string;
 
     const listUsersService = new ListUsersService();
 
-    const users = await listUsersService.execute(company);
+    const users = await listUsersService.execute(company, id);
 
     return res.json({ users: users });
   }
