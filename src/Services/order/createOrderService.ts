d57047d0661f9210e52273f24bbd77480662c9d9
unknown <tiago.rafael019@gmail.com>
2025-03-05 07:58:37 -0300

    Corrigindo criacao de pedidos

--- src/Services/order/createOrderService.ts
+++ src/Services/order/createOrderService.ts
@@ -2,15 +2,27 @@
 import prismaCatalogs from "../../prisma/catalogs";
 
 // Tipagem
+type Items = {
+  productId: string;
+  quantity: number;
+};
+
 interface CreateOrderServiceProps {
   statusId: string;
   client?: string;
   table: number;
+  items: Items[];
   company: string;
 }
 
 class CreateOrderService {
-  async execute({ client, statusId, table, company }: CreateOrderServiceProps) {
+  async execute({
+    client,
+    statusId,
+    table,
+    items,
+    company,
+  }: CreateOrderServiceProps) {
     const prismaClient = company === "catalogs" && prismaCatalogs;
 
     const order = await prismaClient.order.create({
@@ -18,6 +30,12 @@ class CreateOrderService {
         client,
         table,
         status_id: "37a62c52-89d3-4b9e-b070-a9667d631f7b",
+        Items: {
+          create: items.map((item) => ({
+            product_id: item.productId,
+            quantity: item.quantity,
+          })),
+        },
       },
       select: {
         id: true,
@@ -30,6 +48,22 @@ class CreateOrderService {
         },
         created_at: true,
         updated_at: true,
+        Items: {
+          select: {
+            id: true,
+            quantity: true,
+            product: {
+              select: {
+                id: true,
+                name: true,
+                description: true,
+                price: true,
+                promotion: true,
+                discount_price: true,
+              },
+            },
+          },
+        },
       },
     });
 
--- src/controllers/order/createOrderController.ts
+++ src/controllers/order/createOrderController.ts
@@ -6,12 +6,12 @@ import { CreateOrderService } from "../../Services/order/createOrderService";
 
 class CreateOrderController {
   async handle(req: Request, res: Response) {
-    const { client, statusId, table } = req.body;
+    const { client, statusId, table, items } = req.body;
     const company = (req.query.company as string) || "nenhum";
 
     const createOrderService = new CreateOrderService();
 
-    const order = await createOrderService.execute({ client, statusId, table, company });
+    const order = await createOrderService.execute({ client, statusId, table, company, items });
 
     return res.json({ message: "Successfully", order: order });
   }
