64940fb91c61e4af3965538fc4358de826126d30
Admin <tiago.rafael019@gmail.com>
2025-07-31 10:35:53 -0300

    [starting] - Business creation route completed

---
+++ prisma/migrations/20250731123619_optional_field_company/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."companies" ALTER COLUMN "logo_company" DROP NOT NULL;
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { AppController } from './app.controller';
 // Modulo
 import { DatabaseModule } from './database/database.module';
 import { RulesModule } from './rules/rules.module';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [DatabaseModule, RulesModule],
+  imports: [DatabaseModule, RulesModule, CompaniesModule],
   controllers: [AppController],
   providers: [],
 })
