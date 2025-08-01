f92ec32b8f3d348e3bc2d70176f899799e9126d7
Jamille <jamille.araujo.freitas2003@gmail.com>
2025-07-31 21:23:07 -0300

    [FIX] new command in main

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS','HEAD'],
+    allowedHeaders: ['Content-Type','Authorization','Accept','Origin'],
+    // NÃO use credentials: true com origin '*' pois não é permitido
+  });
   app.enableCors();
   app.setGlobalPrefix('v1');
 
