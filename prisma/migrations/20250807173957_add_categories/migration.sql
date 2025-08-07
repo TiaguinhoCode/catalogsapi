af16e434e4965805a4003257ec0019aaa8694e95
Admin <tiago.rafael019@gmail.com>
2025-08-07 14:41:04 -0300

    [RELEASEFIX] - adding another table

---
+++ prisma/migrations/20250807173957_add_categories/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `category_id` to the `products` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."products" ADD COLUMN     "category_id" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "public"."categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
