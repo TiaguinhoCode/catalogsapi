a54f3dcce512a52c9d400958849b5061d92c28c5
TiaguinhoCode <TiaguinhoCode>
2025-09-09 14:30:05 -0300

    [FIX] - Improved API response messages

--- src/utils/common/messages/requestResponse.messages.ts
+++ src/utils/common/messages/requestResponse.messages.ts
@@ -1,7 +1,8 @@
 export const requestResponseMessages = {
   SUCCESSFUL_REQUEST: 'Ok',
-  SUCCESSFUL_CREATION_REQUEST: (name: string) => `${name} criado com sucesso!`,
-  CHANGE_REQUEST: (name: string) => `Dados do ${name} atualizados com sucesso!`,
-  REMOVAL_REQUEST: (name: string) => `Dados do ${name} removido com sucesso!`,
+  SUCCESSFUL_CREATION_REQUEST: (name: string) =>
+    `${name} criado(a) com sucesso!`,
+  CHANGE_REQUEST: (name: string) => `${name} atualizado(a) com sucesso!`,
+  REMOVAL_REQUEST: (name: string) => `${name} removido(a) com sucesso!`,
   ACCESS_NOT_PERMITTED: 'Acesso não permitido',
 };
